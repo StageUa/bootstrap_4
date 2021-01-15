e6894e327175c20a1a5b7e37487eb513a11bac64
Vitalii <stage2903@gmail.com>
2021-01-15 15:22:38 +0200

    изменено:      dist/assets/js/app.js 	изменено:      src/assets/js/modal.js

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,6 +1,7 @@
 const modalBtn = document.querySelectorAll('[data-modal]');
 const body = document.body;
-const modalClose = document.querySelectorAll('.modal');
+const modalClose = document.querySelectorAll('.modal-close');
+const modal = document.querySelectorAll('.modal');
 
 
 modalBtn.forEach(item => {
@@ -8,11 +9,11 @@ modalBtn.forEach(item => {
         let $this = event.currentTarget;
         let modalId = $this.getAttribute('data-modal');
         let modal = document.getElementById(modalId);
-        let modalContent = modal.querySelectorAll('.modal__content');
+        let modalContent = modal.querySelector('.modal__content');
 
         modalContent.addEventListener('click', event => {
             event.stopPropagation();
-        })
+        });
 
         modal.classList.add('show');
         body.classList.add('no-scroll');
@@ -31,7 +32,7 @@ modalClose.forEach(item => {
 
 modal.forEach(item => {
     item.addEventListener('click', event => {
-        let currentModal = event.target;
+        let currentModal = event.currentTarget;
 
         closeModal(currentModal);
     });
